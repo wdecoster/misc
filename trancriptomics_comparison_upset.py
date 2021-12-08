00209df1d20acf9a17cf4899a805fd35ddba8f29
wdecoster <decosterwouter@gmail.com>
2021-12-08 10:14:59 +0100

    also looking at exon count

--- trancriptomics_comparison_upset.py
+++ trancriptomics_comparison_upset.py
@@ -5,8 +5,9 @@ import plotly.express as px
 def main():
     df = pd.read_csv('counts_matrix.tsv', sep="\t").set_index('ids')
     datasets = df.columns.to_list()
-    annot = pd.read_csv("d4905.flair.isoforms.sqantiannot.txt", sep="\t", usecols=["isoform", "gene", "structural_category"])
+    annot = pd.read_csv("d4905.flair.isoforms.sqantiannot.txt", sep="\t", usecols=["isoform", "gene", "structural_category", "exons"])
     annot["identifier"] = annot["isoform"].str.cat(annot['gene'], sep="_")
+    annot["multi-exonic"] = annot["exons"].map(lambda x: "multi-exonic" if x > 1 else "mono-exonic")
     df = df.join(annot.set_index("identifier"))
     
     with open("comparison_plots.html", 'w') as output:
@@ -14,12 +15,13 @@ def main():
                          x="d4905_dircDNArepeat_batch1",
                          y="d4905_pcrcDNA_batch1",
                          color="structural_category",
+                         symbol="multi-exonic",
                          title="direct-cDNA vs PCR-cDNA",
                          hover_data=["gene"])
         html = fig.to_html(full_html=True, include_plotlyjs='cdn')
         output.write(html)
 
-        for min_count in [0,5,10, 20, 50]:
+        for min_count in [0, 5, 10]:
             usp = UpSetPlotly(samples=[get_transcripts(df, c, min_count) for c in datasets],
                               sample_names=datasets
                               )
@@ -28,13 +30,13 @@ def main():
                  .to_html(full_html=True, include_plotlyjs='cdn')
             output.write(html)
 
-        df2 = df[df["structural_category"] != "intergenic"]
-        for min_count in [0,5,10, 20, 50]:
+        df2 = df[(df["structural_category"] != "intergenic") & (df["exons"] != 1)]
+        for min_count in [0, 5, 10]:
             usp = UpSetPlotly(samples=[get_transcripts(df2, c, min_count) for c in datasets],
                               sample_names=datasets
                               )
             html = usp.plot(order_by='decreasing', show_fig=False, return_fig=True) \
-                 .update_layout(title=f"Number of non-intergenic transcripts detected by more than {min_count} supporting reads") \
+                 .update_layout(title=f"Number of multi-exonic non-intergenic transcripts detected by more than {min_count} supporting reads") \
                  .to_html(full_html=True, include_plotlyjs='cdn')
             output.write(html)
 
